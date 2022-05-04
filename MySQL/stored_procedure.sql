c0f4ad0ed1f821c3faf37b48542927761fdbfcb4
Juan Muniain <85519629+munij3@users.noreply.github.com>
2022-05-04 17:35:27 -0500

    Deleted MySQL directory

--- MySQL/stored_procedure.sql
+++
@@ -1,8 +0,0 @@
-delimiter $$ 
-create procedure get_id(in user_n varchar(32))
-begin
-	select user_id 
-    from user_data
-    where user_name = user_n;
-end $$
-delimiter ;
--- MySQL/stored_procedure_2.sql
+++
@@ -1,8 +0,0 @@
-delimiter $$ 
-create procedure get_cantNotes(in song_n varchar(128))
-begin 
-	select note_amount 
-    from music_data
-    where song_name = song_n;
-end $$
-delimiter ;
